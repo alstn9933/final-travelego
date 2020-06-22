998a960a0fbfec9f0651916d42ac1673ef9fcf4a
Hun_0128 <hhaadong1@naver.com>
2020-06-22 13:13:42 +0900

    Hun_companyMemberjoin_0622_1322

--- src/main/java/kr/or/iei/member/model/vo/Member.java
+++ src/main/java/kr/or/iei/member/model/vo/Member.java
@@ -6,11 +6,11 @@ import lombok.Data;
 public class Member {
  private String memberId;
  private String memberPw;
- private String memberNickname;
  private String memberName;
+ private String memberNickname;
  private int age;
- private String gender;
- private String phone;
  private String email;
+ private String phone;
+ private String gender;
  private int memberLevel;
 }
