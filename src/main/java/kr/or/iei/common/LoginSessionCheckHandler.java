ad3f9a75a1796d10a34fa84c39e1388ed9606238
Orcablast <59005472+Orcablast@users.noreply.github.com>
2020-07-03 17:24:20 +0900

    0703 Update - 로그인 세션 체크 AOP 수정(사용자 정의 Exception)

--- src/main/java/kr/or/iei/common/LoginSessionCheckHandler.java
+++ src/main/java/kr/or/iei/common/LoginSessionCheckHandler.java
@@ -1,5 +1,7 @@
 package kr.or.iei.common;
 
+import javax.management.relation.RelationException;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -9,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import kr.or.iei.common.exception.MemberNullException;
 import kr.or.iei.member.controller.MemberController;
 import kr.or.iei.member.model.vo.Member;
 
@@ -19,32 +22,29 @@ public class LoginSessionCheckHandler {
 	MemberController memberController;
 		
 	// Member 객체를 첫번째 매개변수로 참조하는 모든 Service의 모든 메소드
-//	@Pointcut("execution(* kr.or.iei..*Service.*(kr.or.iei.member.model.vo.Member,..))")
-//	public void loginSessionPointcut() {}
-//	
-//	@ExceptionHandler(NullPointerException.class)
-//	public String errorHandler(Model model) {
-//		
-//		model.addAttribute("msg","먼저 로그인 해주세요.");
-//		model.addAttribute("loc","/loginFrm.do");
-//		
-//		return "common/msg";
-//	}
-//	
-//	
-//	// 메소드 실행 전처리
-//	@Before("loginSessionPointcut()")
-//	public void memberHandler(JoinPoint jp) {
-//		
-//		// 메소드의 매개변수들을 args[] 배열로 저장
-//		Object[] args = jp.getArgs();		
-//		
-//		Member member = (Member)args[0];
-//		
-//		// 메소드에서 참조된 매개변수 member가 null일때
-//		if(member == null) {
-//			// 고의적 NullPointerException 발생
-//			member.getMemberId();			
-//		}		
-//	}	
+	@Pointcut("execution(* kr.or.iei..*Service.*(kr.or.iei.member.model.vo.Member,..))")
+	public void loginSessionPointcut() {}
+	
+	@ExceptionHandler(MemberNullException.class)
+	public String errorHandler(Model model) {
+		
+		model.addAttribute("msg","먼저 로그인 해주세요.");
+		model.addAttribute("loc","/loginFrm.do");
+		
+		return "common/msg";
+	}
+	
+	// 메소드 실행 전처리
+	@Before("loginSessionPointcut()")
+	public void memberHandler(JoinPoint jp) {
+		// 메소드의 매개변수들을 args[] 배열로 저장
+		Object[] args = jp.getArgs();		
+		
+		Member member = (Member)args[0];
+		
+		// 메소드에서 참조된 매개변수 member가 null일때
+		if(member == null) {
+			throw new MemberNullException("로그인 세션 조회 실패 : Member");
+		}		
+	}	
 }
