1cce529db1ae5627286cd6470f83cd0fafe20ded
yelliteen <yelliteen@naver.com>
2020-07-03 18:41:35 +0900

    selectList

--- src/main/java/kr/or/iei/common/LoginSessionCheckHandler.java
+++ src/main/java/kr/or/iei/common/LoginSessionCheckHandler.java
@@ -24,7 +24,6 @@ public class LoginSessionCheckHandler {
 	
 	@ExceptionHandler(NullPointerException.class)
 	public String errorHandler(Model model) {
-		
 		model.addAttribute("msg","먼저 로그인 해주세요.");
 		model.addAttribute("loc","/loginFrm.do");
 		
