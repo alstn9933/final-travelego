ac269043441c55193e9e8bc2f10dd4ede04dfe92
JUNGJIYEOB <58426325+JUNGJIYEOB@users.noreply.github.com>
2020-07-06 12:38:08 +0900

    tripBoardWrite start
    시작

--- src/main/java/kr/or/iei/tripBoard/controller/TripBoardController.java
+++ src/main/java/kr/or/iei/tripBoard/controller/TripBoardController.java
@@ -1,5 +1,27 @@
 package kr.or.iei.tripBoard.controller;
 
-public class TripBoardController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.or.iei.tripBoard.model.service.TripBoardService;
+
+@Controller
+public class TripBoardController {
+	@Autowired
+	@Qualifier("tripBoardService")
+	private TripBoardService service;
+	public TripBoardController() {
+		super();
+		System.out.println("TripBoardController 생성완료");
+	}
+	@RequestMapping(value = "/tripBoardWrite.do")
+	public String tripboard() {
+		return "tripBoard/tripBoardWrite";
+	}
+	
+	
+	
+	
 }
--- src/main/java/kr/or/iei/tripBoard/model/dao/TripBoardDao.java
+++ src/main/java/kr/or/iei/tripBoard/model/dao/TripBoardDao.java
@@ -1,5 +1,13 @@
 package kr.or.iei.tripBoard.model.dao;
 
-public class TripBoardDao {
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository("tripBoardDao")
+public class TripBoardDao {
+	@Autowired
+	SqlSession sqlSession;
+	
+	
 }
